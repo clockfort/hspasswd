b9faeb5b6cdc7359ec50e41f94213270e49ccc79
Chris Lockfort <clockfort@csh.rit.edu>
2012-10-04 13:36:32 -0400

    initial commit. Works, has error handling. I need to make a template to make it look sexy, and check if I need to escape some user-facing variables.

---
+++ hspasswd.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Web.Scotty
+
+import Control.Monad.Trans
+import Data.Monoid
+
+import Network.Wai
+
+import Data.Text.Lazy
+import Data.Text.Lazy.Encoding (decodeUtf8)
+import System.Environment
+import System.Process
+import System.Exit
+
+main :: IO ()
+main = scotty 3000 $ do
+
+    get "/" $
+        html $ mconcat ["<form method=POST action=\"cpw\">"
+                       ,"<p>Old password:</p>"
+                       ,"<input type=\"password\" name=password>"
+                       ,"<p>New password:</p>"
+                       ,"<input type=\"password\" name=newPassword>"
+                       ,"<input type=submit>"
+                       ,"</form>"
+                       ]
+
+    post "/cpw" $ do
+	password <- param "password"
+	newPassword <- param "newPassword"
+	username <-  liftIO $ getEnv "REMOTE_USER"
+	(exitcode, stdout, stderr) <- liftIO $ changePassword username password newPassword
+	let success = (exitcode == ExitSuccess)
+	text $ mconcat [ pack $ outputText exitcode username stderr]
+	
+changePassword username password newPassword = do
+	(exitcode, stdout, stderr) <- readProcessWithExitCode "/usr/bin/su" ["-l", username, "-c", "passwd"] (password++"\n"++password++"\n"++newPassword++"\n"++newPassword++"\n")
+	return (exitcode, stdout, stderr)
+
+outputText exitcode username stderr
+	| exitcode == ExitSuccess = "Password successfully changed for "++username++"."
+	| otherwise = "Error: "++stderr
